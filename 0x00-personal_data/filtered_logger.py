fe419e0dba6e05af68ac323a5a8ca8fdaa3d738f
vick-ram <vickramodero6@gmail.com>
2024-08-29 13:04:37 -0700

    logging format correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -38,16 +38,19 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields=None):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """formats the logger"""
-        message = super(RedactingFormatter, self).format(record)
-        redacted = filter_datum(self.fields, self.REDACTION,
-                                message, self.SEPARATOR)
-        return redacted
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.msg,
+            self.SEPARATOR
+        )
+        return super().format(record)
 
 
 def get_logger() -> logging.Logger:
@@ -90,7 +93,8 @@ def filter_data(data: dict) -> dict:
 
 
 def main() -> None:
-    """Main function that retrieves and displays users' data in a filtered format."""
+    """Main function that retrieves and displays users'
+    data in a filtered format."""
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
