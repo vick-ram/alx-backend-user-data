620729a1a5cb9d87fd76e79655172b58ba6f73f9
vick-ram <vickramodero6@gmail.com>
2024-09-03 08:16:06 -0700

    basic class created

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,7 +17,11 @@ auth = None
 auth_type = getenv('AUTH_TYPE')
 
 
-if auth_type == 'auth':
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""""""
+from api.v1.auth.auth import Auth
+
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication class that inherits from Auth """
+    pass
