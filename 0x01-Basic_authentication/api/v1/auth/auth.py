1a30c671e58aa5512be5d34caabee9ab2063dd65
vick-ram <vickramodero6@gmail.com>
2024-09-03 07:43:39 -0700

    require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,19 @@ class Auth:
         excluded_paths: List[str]
     ) -> bool:
         """returns False - path"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
