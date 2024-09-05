f4026f20045c8372d40f483dc7fecec8e0b0038f
vick-ram <vickramodero6@gmail.com>
2024-09-05 12:45:16 -0700

    changes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,16 +14,16 @@ class Auth:
     ) -> bool:
         """returns False - path"""
         if path is None or not excluded_paths:
-        return True
+            return True
 
-    for excluded_path in excluded_paths:
-        if excluded_path.endswith('*'):
-            if path.startswith(excluded_path[:-1]):
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
                 return False
-        elif path == excluded_path:
-            return False
 
-    return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the authorization header from the request."""
