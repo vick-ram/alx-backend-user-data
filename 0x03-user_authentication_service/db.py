af8a341ae405a5a6f772c37adbd3f9f5e5015c52
vickram odero <vickramodero6@gmail.com>
2024-09-10 23:39:53 -0700

    pycode

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,30 +31,30 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> TypeVar[User]:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         return user
-    
+
     def find_user_by(self, **kwargs: Dict[str, str]) -> TypeVar[User]:
         """Finds a user based on arbitrary keyword arguments
-        
+
         Raises:
             NoResultFound: If no user matches the criteria
             InvalidRequestError: If invalid query arguments are passed
         """
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
-            
+
             if user is None:
                 raise NoResultFound
-            
+
             return user
-        
+
         except InvalidRequestError as e:
             raise InvalidRequestError from e
-        
+
     def update_user(self, user_id: int, **kwargs: Dict[str, str]) -> None:
         """Finds user with some specified id and updates
         the user with passed in keyword arguments"""
@@ -63,10 +63,9 @@ class DB:
             for key, value in kwargs.items():
                 if not hasattr(user, key):
                     raise ValueError(f"User has no attribute '{key}'")
-                
+
                 setattr(user, key, value)
             self._session.commit()
-        
+
         except NoResultFound:
             raise ValueError("User not found")
-        
