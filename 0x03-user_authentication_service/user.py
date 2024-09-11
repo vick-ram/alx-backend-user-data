edee63d37cce92eb5584631eb72fe9b40077641e
vickram odero <vickramodero6@gmail.com>
2024-09-10 22:44:48 -0700

    correction

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,15 +5,15 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
-    
+
     def __str__(self) -> str:
         return self.email
-    
