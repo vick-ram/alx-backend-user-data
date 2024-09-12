8955cb1f937d931652020798ab3b8cd7ad0134a0
vickram odero <vickramodero6@gmail.com>
2024-09-12 10:59:03 -0700

    changes to user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,8 +17,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    session_id = Column(String(255), nullable=True)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
 
     def __str__(self) -> str:
