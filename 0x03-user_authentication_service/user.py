870e0c9edfbe9b960b682e81d800a5f2bc54e13c
vickram odero <vickramodero6@gmail.com>
2024-09-12 10:56:45 -0700

    changes to user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,13 +12,14 @@ Base = declarative_base()
 
 
 class User(Base):
+    """User class mapped to the users table."""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=True)
+    reset_token = Column(String(250), nullable=True)
 
     def __str__(self) -> str:
         return self.email
